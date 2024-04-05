79caca9b33765013f105d1c93f04fc35e84c790a
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2024-04-05 09:02:05 -0300

    Espaçamento dos cards

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 import NumeroAleatorio from "./components/NumeroAleatorio"
 import Card from "./components/layout/Card";
+import Cards from "./components/layout/Cards";
 
 export default function App() {
     return (
         <div>
-            <Card titulo="Número Aleatório">
-                <NumeroAleatorio min={1} max={10}/>
-            </Card>
-           
-            <Primeiro/>
-            <ComParametro titulo="Título 1" subtitulo="Título 2" nota={7.1}/>
-            <Fragmento />
+            <Cards>
+                <Card titulo="Número Aleatório">
+                    <NumeroAleatorio min={1} max={10}/>
+                </Card>
+                <Card titulo="Primeiro componente">
+                    <Primeiro/>
+                </Card>
+                <Card titulo="Com parâmetro">
+                    <ComParametro titulo="Título 1" subtitulo="Título 2" nota={7.1}/>
+                </Card>
+                <Card titulo="Fragmentos">
+                    <Fragmento/>
+                </Card>
+            </Cards>  
         </div>
     )
 }
